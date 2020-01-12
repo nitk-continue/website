059b758170bb2a6d54fdecabe610f6f165a0dee3
Aditi Rao <aditiraops@gmail.com>
2020-01-12 22:56:54 +0530

    all basic view templates added. No front end work aside from that is done. Image src still needs to be updated

--- continuesite/homepage/urls.py
+++ continuesite/homepage/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('category/', views.category, name='category'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('textpost/', views.text_post, name='textpost')
 ]
